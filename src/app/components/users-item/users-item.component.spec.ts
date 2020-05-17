df31dd3ca4042e7876251fdb02a422c70011d82c
Andrey Butenko <andrey.butenko@vrpconsulting.com>
2020-05-17 23:54:10 +0300

    feat: added unit tests

--- src/app/components/users-item/users-item.component.spec.ts
+++ src/app/components/users-item/users-item.component.spec.ts
@@ -18,7 +18,7 @@ describe('UsersItemComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
--- src/app/services/users.service.mock.ts
+++ src/app/services/users.service.mock.ts
@@ -1,6 +1,6 @@
 import { of } from 'rxjs';
 
 export const usersServiceMock = {
-  getUsers: jasmine.createSpy('getUsers').and.returnValue(of({ data: 'data'})),
-  getSingleUser: jasmine.createSpy('getSingleUser').and.returnValue(of({ data: 'data'})),
+  getUsers: jasmine.createSpy('getUsers').and.returnValue(of({ data: []})),
+  getSingleUser: jasmine.createSpy('getSingleUser').and.returnValue(of({ data: []})),
 };

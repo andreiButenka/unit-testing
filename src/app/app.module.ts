02a8e7fec2d6553a6df160d9efcfc9ce575eb8b9
Andrey Butenko <andrey.butenko@vrpconsulting.com>
2020-05-17 14:08:06 +0300

    feat: added directive and pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { UsersService } from './services/users.service';
 import { HttpClientModule } from '@angular/common/http';
 import { UserInfoPageComponent } from './pages/user-info-page/user-info-page.component';
 import { UsersPageComponent } from './pages/users-page/users-page.component';
+import { BoldDirective } from './directives/bold.directive';
+import { UppercasePipe } from './pipes/uppercase.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersPageComponent,
     UsersItemComponent,
-    UserInfoPageComponent
+    UserInfoPageComponent,
+    BoldDirective,
+    UppercasePipe
   ],
   imports: [
     BrowserModule,
